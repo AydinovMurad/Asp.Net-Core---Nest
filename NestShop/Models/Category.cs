3cc65d4c63c360a79217b4f27573ff1c357a24d7
AydinovMurad <muradmay@code.edu.az>
2024-04-15 20:54:37 +0400

    ready

---
+++ NestShop/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NestShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Icon { get; set; } = null!;
+        [NotMapped]
+        public IFormFile File { get; set; } 
+        public List<Product>? Products { get; set; }
+    }
+}
